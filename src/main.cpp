6d3d41f4af66dfd9b2a1e57df7c8ef02ed22b240
Ricky Elrod <ricky@elrod.me>
2011-03-17 18:05:39 -0400

    s/spaces/tabs/

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,10 @@ using std::endl;
 
 int main(int argc, char** argv) {
     if(argc < 1) { // impossible, argv[0] is how the program was invoked
-		cout << "Usage: " << argv[0] << endl;
-		return 1;
-	}
+        cout << "Usage: " << argv[0] << endl;
+        return 1;
+    }
 
-	cout << "We do nothing so far!" << endl;
-	return 1;
+    cout << "We do nothing so far!" << endl;
+    return 1;
 }
