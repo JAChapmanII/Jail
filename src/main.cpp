f040b32ab3c7daa6eb41ad1c6f19a8200cfee18c
Jeff Chapman II <JAChapmanII@GMail.com>
2011-03-17 14:16:11 -0400

    Add rather pointless start of main

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main( int argc, char** argv )
+{
+	if( argc < 1 ) // impossible, argv[0] is how the program was invoked
+	{
+		cout << "Usage: " << argv[0] << endl;
+		return 1;
+	}
+
+	cout << "We do nothing so far!" << endl;
+	return 1;
+}
+
