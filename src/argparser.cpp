789f2192cf359d9fc76137c8c7574ea576ded490
Jeff Chapman II <JAChapmanII@GMail.com>
2011-03-18 09:55:47 -0400

    Use initializer lists, implement queue/runCommands methods

--- src/argparser.cpp
+++ src/argparser.cpp
@@ -1,9 +1,23 @@
 #include "argparser.hpp"
 using std::string;
 using std::vector;
+using std::pair;
+
+ArgParser::ArgParser() : // TODO: formatting guidlines here?
+        longCommands(),
+        shortCommands(),
+        hasArgs(),
+        callbackFunctions(),
+        toCall() { // TODO ?
+}
 
 ArgParser::ArgParser(string longCommand, string shortCommand, bool hasArg,
-        ArgParser::CallbackFunction function) {
+        ArgParser::CallbackFunction function) :
+        longCommands(),
+        shortCommands(),
+        hasArgs(),
+        callbackFunctions(),
+        toCall() { // TODO: formatting guidlines here?
     this->add(longCommand, shortCommand, hasArg, function);
 }
 
@@ -19,11 +33,23 @@ void ArgParser::remove(string commandName) {
     // left as excersize for reader
 }
 
+void ArgParser::queue(ArgParser::CallbackFunction function, string argument) {
+    if(function == NULL)
+        return;
+    this->toCall.insert( // TODO: formatting guidlines here?
+        pair<ArgParser::CallbackFunction, string>(function, argument));
+}
+
 int ArgParser::parseArguments(int &argc, char ***argv) {
     // copy-pasta loop? XD wait why can't......
 }
 
 void ArgParser::runCommands() {
+    for(ArgParser::toCallMap::iterator i = this->toCall.begin();
+        i != this->toCall.end(); ++i) // TODO spacing here too?
+        if(i->first != NULL)
+            // call the stored function with its argument
+            (*(i->first))(i->second);
 }
 
 // vim:ts=4 et sw=4 sts=4
