827406ef9456c7de66dc1962442e0e803c6e9553
Jeff Chapman II <JAChapmanII@GMail.com>
2011-03-18 05:14:50 -0400

    Start basic argument parseing routine

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,45 @@
 using std::cout;
 using std::endl;
 
+#include <string>
+using std::string;
+
+#include <vector>
+using std::vector;
+
+void parseCommand(string command);
+
 int main(int argc, char** argv) {
     if(argc < 1) { // impossible, argv[0] is how the program was invoked
         cout << "Usage: " << argv[0] << endl;
         return 1;
     }
 
-    cout << "We do nothing so far!" << endl;
-    return 1;
+    bool isParseCommand = false;
+    string pCommand = "";
+    vector<string> arguments;
+    for(int i = 0; i < argc; ++i)
+    {
+        if((string)argv[i] == (string)"--parse-command")
+        {
+            isParseCommand = true;
+            if(i < argc - 1)
+                // set pCommand to be the next argument, and thes skip it
+                pCommand = argv[i++ + 1];
+        }
+    }
+
+
+    if( isParseCommand )
+        parseCommand(pCommand);
+
+    return 0;
+}
+
+void parseCommand(string command)
+{
+    cout << "Looks like a fine command to me!" << endl;
+    return;
 }
 
 // vim:ts=4 et sw=4 sts=4
