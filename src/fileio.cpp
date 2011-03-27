a2714d40a15d37805c27b3ff92dcccf89f293f1d
Jeff Chapman II <JAChapmanII@GMail.com>
2011-03-27 04:40:34 -0400

    Start implementing an file IO abstraction layer

---
+++ src/fileio.cpp
@@ -0,0 +1,69 @@
+#include "fileio.hpp"
+using std::string;
+using std::fstream;
+using std::ios;
+
+FileIO::FileIO(string iFileName) : // TODO: formatting guidelines
+        fileName(iFileName),
+        file(),
+        length(-1),
+        data(NULL) {
+}
+
+FileIO::~FileIO() {
+    this->clear();
+    this->close();
+}
+
+void FileIO::open() {
+    this->clear();
+    this->close();
+    this->file.open(this->fileName.c_str(), fstream::in | fstream::out);
+}
+
+void FileIO::close() {
+    this->file.close();
+}
+
+int FileIO::read() {
+    if(!this->file.good())
+        return -1;
+
+    this->file.seekg(0, ios::end);
+    this->length = this->file.tellg();
+    this->file.seekg(0, ios::beg);
+
+    // TODO: these things could fail
+    this->data = new char[this->length];
+    this->file.read(this->data, this->length);
+
+    return this->length;
+}
+
+int FileIO::getLength() const {
+    return this->length;
+}
+
+char *FileIO::getData() {
+    return this->data;
+}
+
+void FileIO::clear() {
+    if(this->length < 0)
+        return;
+
+    delete[] this->data;
+    this->length = -1;
+}
+
+string FileIO::getFileName() const {
+    return this->fileName;
+}
+
+void FileIO::setFileName(string nFileName) {
+    this->clear();
+    this->close();
+    this->fileName = nFileName;
+}
+
+// vim:ts=4 et sw=4 sts=4
---
+++ src/fileio.hpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+
+class FileIO {
+    public:
+        FileIO(std::string iFileName);
+        ~FileIO();
+
+        void open();
+        void close();
+
+        int read();
+        int getLength() const;
+        char *getData();
+
+        void clear();
+
+        std::string getFileName() const;
+        void setFileName(std::string nFileName);
+
+    protected:
+        // TODO: implement these, or not?
+        FileIO(const FileIO &rhs);
+        FileIO &operator=(const FileIO &rhs);
+
+        std::string fileName;
+        std::fstream file;
+
+        int length;
+        char *data;
+};
+
+// vim:ts=4 et sw=4 sts=4
