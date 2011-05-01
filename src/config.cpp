2e038e4ec78109e6d5a6ff8034b92a049c77fca0
Jeff Chapman II <jac@JAChapmanII.net>
2011-05-01 18:36:30 -0400

    Put everything into the Config namespace

--- src/config.cpp
+++ src/config.cpp
@@ -4,58 +4,60 @@ using std::string;
 #include <sstream>
 using std::istringstream;
 
-bool beVerbose = false;
-bool beQuiet = false;
-bool doDump = false;
-bool hexMode = false;
-int width = 80;
+namespace Config {
+    bool beVerbose = false;
+    bool beQuiet = false;
+    bool doDump = false;
+    bool hexMode = false;
+    int outWidth = 80;
 
-int setVerbose() {
-    beVerbose = true;
-    beQuiet = false;
-}
+    int setVerbose() {
+        beVerbose = true;
+        beQuiet = false;
+    }
 
-int setQuiet() {
-    beQuiet = true;
-    beVerbose = false;
-}
+    int setQuiet() {
+        beQuiet = true;
+        beVerbose = false;
+    }
 
-int setDump() {
-    doDump = true;
-}
+    int setDump() {
+        doDump = true;
+    }
 
-int setHexMode() {
-    hexMode = true;
-}
+    int setHexMode() {
+        hexMode = true;
+    }
 
-int setHexDump() {
-    hexMode = true;
-    doDump = true;
-}
+    int setHexDump() {
+        hexMode = true;
+        doDump = true;
+    }
 
-bool isVerbose() {
-    return beVerbose;
-}
-bool isVuiet() {
-    return beQuiet;
-}
-bool isDump() {
-    return doDump;
-}
-bool isHexMode() {
-    return hexMode;
-}
+    bool isVerbose() {
+        return beVerbose;
+    }
+    bool isVuiet() {
+        return beQuiet;
+    }
+    bool isDump() {
+        return doDump;
+    }
+    bool isHexMode() {
+        return hexMode;
+    }
 
-int setWidth(string w) {
-    istringstream ss(w);
-    int tmp = 0;
-    if(ss >> tmp)
-        return width = (tmp > 3) ? tmp : 3;
-    else
-        return width;
-}
-int getWidth() {
-    return width;
+    int setWidth(string w) {
+        istringstream ss(w);
+        int tmp = 0;
+        if(ss >> tmp)
+            return outWidth = (tmp > 3) ? tmp : 3;
+        else
+            return outWidth;
+    }
+    int getWidth() {
+        return outWidth;
+    }
 }
 
 // vim:ts=4 et sw=4 sts=4
--- src/config.hpp
+++ src/config.hpp
@@ -3,19 +3,21 @@
 
 #include <string>
 
-int setVerbose();
-int setQuiet();
-int setDump();
-int setHexMode();
-int setHexDump();
+namespace Config {
+    int setVerbose();
+    int setQuiet();
+    int setDump();
+    int setHexMode();
+    int setHexDump();
 
-bool isVerbose();
-bool isQuiet();
-bool isDump();
-bool isHexMode();
+    bool isVerbose();
+    bool isQuiet();
+    bool isDump();
+    bool isHexMode();
 
-int setWidth(std::string w);
-int getWidth();
+    int setWidth(std::string w);
+    int getWidth();
+}
 
 #endif // CONFIG_HPP
 // vim:ts=4 et sw=4 sts=4
