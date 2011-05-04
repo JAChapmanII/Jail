ac7dd85e61f5d8526c741607dc1250cf3456028e
Jeff Chapman II <jac@JAChapmanII.net>
2011-05-04 15:32:50 -0400

    Add comments to the FileIO class

--- src/fileio.hpp
+++ src/fileio.hpp
@@ -4,21 +4,34 @@
 #include <fstream>
 #include <string>
 
+/**
+ * FileIO is used to read and write to and from files on disk.
+ */
 class FileIO {
     public:
+        /// Construct a FileIO with a backing file
         FileIO(std::string iFileName);
+        /// Safely destroy a FileIO
         ~FileIO();
 
+        /// Open backing file for manipulation
         void open();
+        /// Close backing file, locking manipulation operations
         void close();
 
+        /// Read in the backing file
         int read();
+        /// Get the length of the currently read data buffer
         int getLength() const;
+        /// Get a pointer to the internal representation of the read data
         char *getData();
 
+        /// Clear the data buffer
         void clear();
 
+        /// Get the backing filename
         std::string getFileName() const;
+        /// Set this FileIO to use a different backing file
         void setFileName(std::string nFileName);
 
     protected:
